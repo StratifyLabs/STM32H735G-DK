97cfa794ef76995f5e696d5ad2d8328aaddbba7b
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-08-03 20:10:52 -0500

    port to CMakeSDK v2.0

--- bootstrap.cmake
+++ bootstrap.cmake
@@ -1,22 +1,23 @@
-cmake_minimum_required (VERSION 3.19)
-
-set(SDK_DIRECTORY ${CMAKE_SOURCE_DIR}/SDK)
-set(SDK_PATH ${SDK_DIRECTORY}/StratifyLabs-SDK)
-
-file(MAKE_DIRECTORY ${SDK_PATH})
+set(SDK_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/SDK)
 set(DEPENDENCIES_DIRECTORY ${SDK_DIRECTORY}/dependencies)
 
+file(MAKE_DIRECTORY ${DEPENDENCIES_DIRECTORY})
+file(REMOVE_RECURSE ${DEPENDENCIES_DIRECTORY}/CMakeSDK)
 
-if(NOT EXISTS ${DEPENDENCIES_DIRECTORY}/CMakeSDK/CMakeLists.txt)
-	file(MAKE_DIRECTORY ${DEPENDENCIES_DIRECTORY})
-	execute_process(
-		COMMAND git clone --branch v1.2 https://github.com/StratifyLabs/CMakeSDK.git
-		WORKING_DIRECTORY ${DEPENDENCIES_DIRECTORY}
-		)
-endif()
+message(STATUS "cloning CMakeSDK project")
 
 execute_process(
-	COMMAND cmake -DSDK_DIRECTORY=${SDK_DIRECTORY} -P ${DEPENDENCIES_DIRECTORY}/CMakeSDK/scripts/bootstrap.cmake
+	COMMAND git clone --branch v2.0 https://github.com/StratifyLabs/CMakeSDK.git
+	WORKING_DIRECTORY ${DEPENDENCIES_DIRECTORY}
+)
+
+execute_process(
+	COMMAND git clone --branch v2.0 https://github.com/StratifyLabs/CMakeSDK.git
+	WORKING_DIRECTORY ${DEPENDENCIES_DIRECTORY}
+	)
+
+execute_process(
+	COMMAND cmake -DCMSDK_SDK_PATH=${SDK_DIRECTORY} -P ${DEPENDENCIES_DIRECTORY}/CMakeSDK/scripts/sos-bootstrap.cmake
 	)
 
 
